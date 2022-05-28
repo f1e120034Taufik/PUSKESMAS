360ccc3df1e408142e0cbbeba677dccb5cda2046
Zikri Hanafi <fabbmad@gmail.com>
2022-05-28 16:00:08 +0700

    Update Database

--- db_puskesmas.sql
+++ db_puskesmas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Bulan Mei 2022 pada 06.08
+-- Waktu pembuatan: 28 Bulan Mei 2022 pada 10.58
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 8.0.13
 
@@ -55,13 +55,21 @@ CREATE TABLE `dokter` (
   `Id_dokter` int(11) NOT NULL,
   `Nama_dokter` varchar(20) NOT NULL,
   `Alamat_dokter` varchar(100) NOT NULL,
-  `Hp_dokter` int(11) NOT NULL,
+  `Hp_dokter` varchar(12) NOT NULL,
   `Email_dokter` varchar(30) NOT NULL,
   `Tgl_lahir_dokter` date NOT NULL,
-  `Jenis_kelamin_dokter` enum('L','P','','') NOT NULL,
+  `Jenis_kelamin_dokter` enum('Laki-laki','Perempuan','','') NOT NULL,
   `Spesialis` varchar(20) NOT NULL,
-  `Jam_kerja` time NOT NULL
+  `Jam_kerja` enum('08:00 - 16:00','16:00 - 23:00','Belum Pasti') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `dokter`
+--
+
+INSERT INTO `dokter` (`Id_dokter`, `Nama_dokter`, `Alamat_dokter`, `Hp_dokter`, `Email_dokter`, `Tgl_lahir_dokter`, `Jenis_kelamin_dokter`, `Spesialis`, `Jam_kerja`) VALUES
+(10, 'Zikri Hanafi', 'Villa Kenali Permai Blok I4', '0896', 'fabbmad@gmail.com', '2002-09-21', 'Laki-laki', 'Jantung', '16:00 - 23:00'),
+(12, 'Hurum Maqshura', 'Mayang Mangurai', '08965', 'maqshura@gmail.com', '2002-11-15', 'Perempuan', 'Jantung', '08:00 - 16:00');
 
 -- --------------------------------------------------------
 
@@ -77,6 +85,15 @@ CREATE TABLE `obat` (
   `Kuantitas` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`Id_obat`, `Nama_obat`, `Jenis_obat`, `Kandungan`, `Kuantitas`) VALUES
+(2, 'Trimate-E Supplemen', 'Suplemen Neurotropik', 'Benfotiamine 25 mg, pyridoxine HCl 25 mg, hydroxocobalamin 50 mg, dl-α tocopheryl acetate 50 mg', 19),
+(4, 'Trimate-C Supplemen', 'Suplemen Neurotropik', 'Benfotiamine 25 mg, pyridoxine HCl 25 mg, hydroxocobalamin 50 mg, dl-α tocopheryl acetate 50 mg', 10),
+(5, 'Trimate-D Supplemen', 'Suplemen Neurotropik', 'Benfotiamine 25 mg, pyridoxine HCl 25 mg, hydroxocobalamin 50 mg, dl-α tocopheryl acetate 50 mg', 15);
+
 -- --------------------------------------------------------
 
 --
@@ -84,16 +101,24 @@ CREATE TABLE `obat` (
 --
 
 CREATE TABLE `pasien` (
-  `id_pasien` int(11) NOT NULL,
+  `Id_pasien` int(11) NOT NULL,
   `Nama_pasien` varchar(20) NOT NULL,
   `Alamat_pasien` varchar(100) NOT NULL,
-  `Hp_pasien` int(11) NOT NULL,
+  `Hp_pasien` varchar(12) NOT NULL,
   `Tgl_lahir_pasien` date NOT NULL,
-  `Jenis_kelamin_pasien` enum('L','P','','') NOT NULL,
+  `Jenis_kelamin_pasien` enum('Laki-laki','Perempuan','','') NOT NULL,
   `R_penyakit_sebelumnya` varchar(50) NOT NULL,
   `Dr_perawat` int(11) NOT NULL,
   `Obat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `pasien`
+--
+
+INSERT INTO `pasien` (`Id_pasien`, `Nama_pasien`, `Alamat_pasien`, `Hp_pasien`, `Tgl_lahir_pasien`, `Jenis_kelamin_pasien`, `R_penyakit_sebelumnya`, `Dr_perawat`, `Obat`) VALUES
+(20, 'Rafli Ramadhan', 'Depan Toko Bangunan masuk Gang', '0895', '2002-03-19', 'Laki-laki', 'Batuk2', 12, 2),
+(21, 'Indah Purnamasari', 'Di Kosan dekat Unja masuk gang depan dikit', '0892', '2000-04-12', 'Perempuan', 'Batuk2, Dak tau lagi habis tu', 10, 4);
 
 -- --------------------------------------------------------
 
@@ -103,9 +128,18 @@ CREATE TABLE `pasien` (
 
 CREATE TABLE `ruang` (
   `Id_ruang` int(11) NOT NULL,
+  `No_ruangan` varchar(12) NOT NULL,
   `Pasien` int(11) NOT NULL,
   `Dokter` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `ruang`
+--
+
+INSERT INTO `ruang` (`Id_ruang`, `No_ruangan`, `Pasien`, `Dokter`) VALUES
+(1, '105', 21, 10),
+(2, '106', 20, 12);
 
 --
 -- Indexes for dumped tables
@@ -133,7 +167,7 @@ ALTER TABLE `obat`
 -- Indeks untuk tabel `pasien`
 --
 ALTER TABLE `pasien`
-  ADD PRIMARY KEY (`id_pasien`),
+  ADD PRIMARY KEY (`Id_pasien`),
   ADD KEY `DOKTER` (`Dr_perawat`),
   ADD KEY `OBAT` (`Obat`);
 
@@ -141,6 +175,7 @@ ALTER TABLE `pasien`
 -- Indeks untuk tabel `ruang`
 --
 ALTER TABLE `ruang`
+  ADD PRIMARY KEY (`Id_ruang`),
   ADD KEY `PASIEN` (`Pasien`),
   ADD KEY `DOKTER_R` (`Dokter`);
 
@@ -158,19 +193,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT untuk tabel `dokter`
 --
 ALTER TABLE `dokter`
-  MODIFY `Id_dokter` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_dokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT untuk tabel `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `Id_obat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT untuk tabel `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id_pasien` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id_pasien` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `ruang`
+--
+ALTER TABLE `ruang`
+  MODIFY `Id_ruang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
